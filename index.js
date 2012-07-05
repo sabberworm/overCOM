95594fc58dc564e1ffaca011bf1a6cfb797b6d2d
Raphael Schweikert <any@sabberworm.com>
2012-07-05 13:32:11 +0200

    Add update method
    Allows a COMObject to update its properties.
    COM objects are now EventEmitters, emitting the “propertiesChanged”
    event when an update actually changed something.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var exec = require('child_process').exec;
+var EventEmitter = require('events').EventEmitter;
 var sax = require('sax');
 
 var objectIndex = 0;
@@ -16,6 +17,70 @@ function queryPS(comType, query, callback) {
 		}
 		callback(null, stdout.replace(/^[^<]+/, '').replace(/(\r\n)+/g, ''));
 	}).stdin.end();
+}
+
+function parseObject(text, callback) {
+	var result = {
+		properties: {},
+		subObjects: [],
+		type: null
+	};
+	var parser = sax.parser(true);
+	var currentNode = null;
+	var error = null;
+	parser.onopentag = function(node) {
+		node.text = '';
+		node.parentNode = currentNode;
+		currentNode = node;
+	};
+	parser.onclosetag = function() {
+		if(currentNode.name === 'Object') {
+			result.type = currentNode.attributes.Type;
+		} else if(currentNode.name === 'Property') {
+			if(!currentNode.attributes.Type) {
+				error = new Error('Property '+currentNode.attributes.Name+' does not have a type');
+				return;
+			}
+			var propertyType = currentNode.attributes.Type.replace(/^System\./, '');
+			var name = currentNode.attributes.Name;
+			var value = currentNode.text;
+
+			if(propertyType === '__ComObject' || propertyType === 'Object') {
+				result.subObjects.push(name);
+			} else {
+				if(propertyType === 'String') {
+					//String
+					value = value;
+				} else if(propertyType === 'Decimal') {
+					value = parseFloat(value);
+				} else if(propertyType === 'Boolean') {
+					value = value === 'True';
+				} else if(propertyType === 'DateTime') {
+					value = new Date(value+'Z');
+				} else if(propertyType === 'Nil') {
+					value = null;
+				} else if(propertyType.indexOf('Int') === 0) {
+					value = parseInt(value, 10);
+				} else {
+					error = new Error('Unknown attribute type '+propertyType);
+				}
+				result.properties[name] = value;
+			}
+		}
+		currentNode = currentNode.parentNode;
+	};
+	parser.ontext = function(text) {
+		if(currentNode) {
+			currentNode.text += text;
+		}
+	};
+	parser.onerror = function(e) {
+		error = e;
+	};
+	parser.onend = function() {
+		callback(error, result);
+	};
+	parser.write(text).end();
 }
 
 function COMObject(session, path, parent, type, properties, subObjects) {
@@ -43,78 +108,33 @@ function COMObject(session, path, parent, type, properties, subObjects) {
 	}
 }
 
-COMObject.prototype.fullPath = function() {
+COMObject.prototype.__proto__ = EventEmitter.prototype;
+
+COMObject.prototype.fullPath = function(query, asArray) {
+	var result = [];
 	if(this.parent) {
-		return this.parent.fullPath() + this.path + '.';
+		result = result.concat(this.parent.fullPath(null, true));
 	}
 	if(this.path) {
-		return this.path + '.';
+		result.push(this.path);
 	}
-	return '';
+	if(query) {
+		result.push(query);
+	}
+	if(asArray) {
+		return result;
+	}
+	return result.join('.');
 };
 
 COMObject.prototype.parseResult = function(text, query, callback) {
 	var _this = this;
-	var parser = sax.parser(true);
-	var currentNode = null;
-	var properties = {};
-	var subObjects = [];
-	var error = null;
-	var type = null;
-	parser.onopentag = function(node) {
-		node.text = '';
-		node.parentNode = currentNode;
-		currentNode = node;
-	};
-	parser.onclosetag = function() {
-		if(currentNode.name === 'Object') {
-			type = currentNode.attributes.Type;
-		} else if(currentNode.name === 'Property') {
-			if(!currentNode.attributes.Type) {
-				error = new Error('Property '+currentNode.attributes.Name+' does not have a type');
-				return;
-			}
-			var propertyType = currentNode.attributes.Type.replace(/^System\./, '');
-			var name = currentNode.attributes.Name;
-			var value = currentNode.text;
-
-			if(propertyType === '__ComObject' || propertyType === 'Object') {
-				subObjects.push(name);
-			} else {
-				if(propertyType === 'String') {
-					//String
-					value = value;
-				} else if(propertyType === 'Decimal') {
-					value = parseFloat(value);
-				} else if(propertyType === 'Boolean') {
-					value = value === 'True';
-				} else if(propertyType === 'DateTime') {
-					value = new Date(value+'Z');
-				} else if(propertyType === 'Nil') {
-					value = null;
-				} else if(propertyType.indexOf('Int') === 0) {
-					value = parseInt(value, 10);
-				} else {
-					error = new Error('Unknown attribute type '+propertyType);
-				}
-				properties[name] = value;
-			}
+	parseObject(text, function(error, obj) {
+		if(error) {
+			return callback(error);
 		}
-		currentNode = currentNode.parentNode;
-	};
-	parser.ontext = function(text) {
-		if(currentNode) {
-			currentNode.text += text;
-		}
-	};
-	parser.onerror = function(e) {
-		error = e;
-	};
-	parser.onend = function() {
-		var result = error ? null : new COMObject(_this.session, query, _this, type, properties, subObjects);
-		callback(error, result);
-	};
-	parser.write(text).end();
+		callback(null, new COMObject(_this.session, query, _this, obj.type, obj.properties, obj.subObjects));
+	});
 };
 
 
@@ -153,7 +173,32 @@ COMObject.prototype.queryMethod = function(methodName) {
 };
 
 COMObject.prototype.queryText = function(query, callback) {
-	queryPS(this.session.comType, this.fullPath()+query, callback);
+	queryPS(this.session.comType, this.fullPath(query), callback);
+};
+
+COMObject.prototype.update = function() {
+	var _this = this;
+	this.queryText(null, function(error, text) {
+		if(error) {
+			return;
+		}
+		parseObject(text, function(error, obj) {
+			if(error) {
+				return;
+			}
+			var updatedProperties = [];
+			var propertyName;
+			for(propertyName in obj.properties) {
+				if(_this.properties[propertyName] !== obj.properties[propertyName]) {
+					updatedProperties.push(propertyName);
+					_this.properties[propertyName] = obj.properties[propertyName];
+				}
+			}
+			if(updatedProperties.length) {
+				_this.emit('propertiesChanged', updatedProperties);
+			}
+		});
+	});
 };
 
 function COMSession(comType) {
